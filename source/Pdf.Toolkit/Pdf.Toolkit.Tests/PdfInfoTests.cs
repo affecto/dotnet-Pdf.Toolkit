64cfdb421bc0f8ac03a1211c4a79e98eb0aae0ee
Ville Ruuskanen <ville.ruuskanen@affecto.com>
2016-02-18 13:42:27 +0200

    Added PdfMerger and PdfInfo

---
+++ source/Pdf.Toolkit/Pdf.Toolkit.Tests/PdfInfoTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Affecto.Pdf.Toolkit.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Affecto.Pdf.Toolkit.Tests
+{
+    [TestClass]
+    public class PdfInfoTests
+    {
+        private PdfInfo info;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            info = new PdfInfo(@"Resources\A3 Test Page.pdf");
+        }
+
+        [TestMethod]
+        public void NumberOfPages()
+        {
+            Assert.AreEqual(1, info.NumberOfPages);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ReadingPdfInfoFailedException))]
+        public void FileDoesNotExist_ThrowsException()
+        {
+           new PdfInfo("foo.pdf");
+        }
+    }
+}
---
+++ source/Pdf.Toolkit/Pdf.Toolkit/PdfInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Affecto.Pdf.Toolkit.Exceptions;
+using iTextSharp.text.pdf;
+
+namespace Affecto.Pdf.Toolkit
+{
+    public class PdfInfo
+    {
+        public int NumberOfPages { get; private set; }
+
+        public PdfInfo(string file)
+        {
+            PdfReader reader = null;
+
+            try
+            {
+                reader = new PdfReader(file);
+
+                NumberOfPages = reader.NumberOfPages;
+            }
+            catch (Exception e)
+            {
+                throw new ReadingPdfInfoFailedException($"Failed to read information from pdf file {file}", e);
+            }
+            finally
+            {
+                reader?.Close();
+            }
+        }
+    }
+}
