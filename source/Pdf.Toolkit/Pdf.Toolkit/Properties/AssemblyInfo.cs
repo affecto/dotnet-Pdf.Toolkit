cf3e8612a6a4d95f06a9ea2f95d7b5b44c357c60
Ville Ruuskanen <ville.ruuskanen@affecto.com>
2016-02-18 08:44:51 +0200

    Solution and project created

---
+++ source/Pdf.Toolkit/Pdf.Toolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Affecto.Pdf.Toolkit")]
+[assembly: AssemblyDescription("Tools for editing pdf files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Affecto")]
+[assembly: AssemblyProduct("Affecto.Pdf.Toolkit")]
+[assembly: AssemblyCopyright("Copyright © Affecto 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cbbca082-5fb4-4c27-8814-9b43a5ff056b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-prerelease01")]
